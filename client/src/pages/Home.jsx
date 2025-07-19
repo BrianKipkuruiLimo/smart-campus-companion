6563be360ab7437c0e6469e85421b2a968723dc7
BrianKipkuruiLimo <lelmetbrian4@gmail.com>
2025-07-19 12:07:30 +0300

    Updated Navbar and Home pages; added components folder

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from '../context/Navbar'
+import Header from '../components/Header'
 
 
 const Home = ()=> {
@@ -14,8 +15,10 @@ Welcome to the Home Page!
 }
 =======
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[url('/bg_img.png')] bg-cover bg-center">
+
       <Navbar />
+      <Header/>
     </div>
   )
 }
